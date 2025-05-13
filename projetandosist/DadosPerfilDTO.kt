a391ad37e2414889be84b8b51fa47b766c4da590
MatheusHaliski <120330198+MatheusHaliski@users.noreply.github.com>
2025-05-13 17:10:05 -0300

    Delete projetandosist directory

--- projetandosist/DadosPerfilDTO.kt
+++
@@ -1,8 +0,0 @@
-package com.example.projetandosist
-
-data class DadosPerfilDTO(
-    val nome: String,
-    val email: String,
-    val senha: String?,
-    val emailUsuario: String
-)
--- projetandosist/PessoaRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.projetandosist
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PessoaRepository : JpaRepository<Pessoa, Long> {
-    fun findByEmailAndSenha(email: String, senha: String): Pessoa?
-    fun findByEmail(email: String): Pessoa?
-    fun findByNome(nome: String): Pessoa?
-    }
